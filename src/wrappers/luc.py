f09b3a53906d3aa8d6fca68341354c6a5c0537e5
Zhiming <zhiminghuang@uvic.ca>
2023-05-15 00:27:52 -0700

    update

---
+++ src/wrappers/luc.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from subprocess import check_call
+
+import arg_parser
+import context
+from helpers import kernel_ctl
+
+
+
+def main():
+    args = arg_parser.receiver_first()
+
+    if args.option == 'deps':
+        print 'iperf'
+        return
+
+
+    if args.option == 'receiver':
+        cmd = ['iperf', '-Z', 'ccp', '-s', '-p', args.port]
+        check_call(cmd)
+        return
+
+    if args.option == 'sender':
+        cmd = ['iperf', '-Z', 'ccp', '-c', args.ip, '-p', args.port,
+               '-t', '75']
+        check_call(cmd)
+        return
+
+
+if __name__ == '__main__':
+    main()
